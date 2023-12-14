98ae74a6f5974dd37a65335988ed810dffe182d6
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-12-14 22:17:01 +0300

    add 5 stars

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-import time
+import asyncio
 import curses
+import time
+
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
 
 def draw(canvas):
     curses.curs_set(0)
-    row, column = (5, 20)
+    canvas.border('|', '|', '-', '-', '+', '+', '+', '+')
+    coroutines = [
+        blink(canvas, 5, 10),
+        blink(canvas, 5, 15),
+        blink(canvas, 5, 20),
+        blink(canvas, 5, 25),
+        blink(canvas, 5, 30)
+    ]
+
     while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
+        for coroutine in coroutines:
+            coroutine.send(None)
         canvas.refresh()
-        time.sleep(2)
-
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+        time.sleep(1)
 
 if __name__ == '__main__':
     curses.wrapper(draw)
