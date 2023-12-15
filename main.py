68258772e1be007622476ac5953489e1deddb28c
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-12-15 15:00:03 +0300

    star animation fix

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import asyncio
 import curses
 import time
 
+
+TIC_TIMEOUT = 0.1
+
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        for x in range(20):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for x in range(3):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        for x in range(5):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for x in range(3):
+            await asyncio.sleep(0)
 
 
 def draw(canvas):
@@ -32,7 +39,7 @@ def draw(canvas):
         for coroutine in coroutines:
             coroutine.send(None)
         canvas.refresh()
-        time.sleep(1)
+        time.sleep(TIC_TIMEOUT)
 
 if __name__ == '__main__':
     curses.wrapper(draw)
