f3a7c450804cbc5a2d95af97d72e14811beb9849
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2024-01-09 17:43:50 +0300

    added physics of spaceship motion

--- rocket_animation.py
+++ rocket_animation.py
@@ -2,11 +2,14 @@ import asyncio
 import itertools
 
 from curses_tools import get_frame_size, draw_frame, read_controls
+from physics import update_speed
 
 
 async def animate_spaceship(canvas, row, column, frames):
     cycle = itertools.cycle(frames)
     rows_direction, columns_direction = 0, 0
+    row_speed, column_speed = 0, 0
+
     first_frame = frames[0]
     rows_ships, columns_ships = get_frame_size(first_frame)
     start_row = (row - rows_ships) // 2
@@ -17,9 +20,10 @@ async def animate_spaceship(canvas, row, column, frames):
 
         for _ in range(2):
             rows_ships, columns_ships = get_frame_size(frame)
+            row_speed, column_speed = update_speed(row_speed, column_speed, rows_direction, columns_direction)
 
-            start_row += rows_direction
-            start_column += columns_direction
+            start_row += row_speed
+            start_column += column_speed
 
             start_row = max(0, min(start_row, row - rows_ships))
             start_column = max(0, min(start_column, column - columns_ships))
